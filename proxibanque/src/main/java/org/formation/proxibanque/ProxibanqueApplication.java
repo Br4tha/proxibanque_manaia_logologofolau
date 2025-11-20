9ecbabc54aa9721d8b80b335ddbf31d67163427f
Manaia Logologofolau <manaia.logologofolau@epita.fr>
2025-11-20 11:00:41 +0100

    first commit

---
+++ proxibanque/src/main/java/org/formation/proxibanque/ProxibanqueApplication.java
@@ -0,0 +1,13 @@
+package org.formation.proxibanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxibanqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxibanqueApplication.class, args);
+    }
+
+}
